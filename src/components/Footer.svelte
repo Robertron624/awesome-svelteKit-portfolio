3f86a0aeddf9017f2df9e2771ed307a172b2925b
Roberto Ramirez <robert.624@hotmail.com>
2023-10-11 22:03:16 -0500

    feat: footer section

---
+++ src/components/Footer.svelte
@@ -0,0 +1,45 @@
+<footer
+    class="py-20 sm:py-32 bg-black border-t border-solid border-violet-950 flex flex-col gap-4 sm:gap-8 justify-center items-center"
+>
+    <p class="px-4 py-2 bg-white text-slate-950 font-medium">
+        Connect with me &darr;
+    </p>
+    <div class="flex flex-col items-center justify-center gap-4">
+        <p>
+            <b class="pr-2">Email</b>
+            robert.624@hotmail.com
+        </p>
+        <p>
+            <b class="pr-2">Github</b>
+            <a
+                href="https://github.com/Robertron624"
+                class="text-violet-400 inline-flex gap-1"
+                target="_blank"
+            >
+                Robertron624 <sub
+                    ><span class="text-xs scale-75 pl-0.5">
+                        <i
+                            class="fa-solid fa-arrow-up-right-from-square text-xs scale-[75%]"
+                        />
+                    </span>
+                </sub>
+            </a>
+        </p>
+        <p>
+            <b class="pr-2">LinkedIn</b>
+            <a
+                href="https://github.com/Robertron624"
+                class="text-violet-400 inline-flex gap-1"
+                target="_blank"
+            >
+                Roberto Ramirez <sub
+                    ><span class="text-xs scale-75 pl-0.5">
+                        <i
+                            class="fa-solid fa-arrow-up-right-from-square text-xs scale-[75%]"
+                        />
+                    </span>
+                </sub>
+            </a>
+        </p>
+    </div>
+</footer>
